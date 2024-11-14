794f6b3700d8b1cbd8421795414799b0b2e97654
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-14 22:47:42 +0100

    simple event handling

--- src/main/java/dev/lpa/udemy430events/HelloController.java
+++ src/main/java/dev/lpa/udemy430events/HelloController.java
@@ -2,13 +2,23 @@ package dev.lpa.udemy430events;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
+
   @FXML
-  private Label welcomeText;
+  private TextField nameField;
 
   @FXML
   protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
+    System.out.println("Hello, " + nameField.getText());
+//    nameField.setText("Welcome to JavaFX Application!");
+  }
+
+  public void onEnterEmptyTextField() {
+    if (nameField.getText().equalsIgnoreCase("Enter name")) {
+      nameField.setText("");
+    }
   }
 }
