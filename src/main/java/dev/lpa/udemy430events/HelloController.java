9b902e0d2e65693f3b578f9695a59d48b8fca9eb
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-15 18:46:55 +0100

    use Background Thread & Platform.runLater

--- src/main/java/dev/lpa/udemy430events/HelloController.java
+++ src/main/java/dev/lpa/udemy430events/HelloController.java
@@ -1,9 +1,11 @@
 package dev.lpa.udemy430events;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
@@ -16,6 +18,8 @@ public class HelloController {
   private Button byeButton;
   @FXML
   private CheckBox ourCheckBox;
+  @FXML
+  private Label ourLabel;
 
   @FXML
   public void initialize() {
@@ -25,19 +29,44 @@ public class HelloController {
 
   @FXML
   protected void onButtonClicked(ActionEvent e) {
-    if(e.getSource().equals(helloButton)) {
+
+    ourLabel.setText("Nothing has happened");
+
+    if (e.getSource().equals(helloButton)) {
       System.out.println("Hello, " + nameField.getText());
     } else if (e.getSource().equals(byeButton)) {
       System.out.println("Bye, " + nameField.getText());
     }
 
-    // Simulate hanging UI Thread
-    try {
-      Thread.sleep(10_000);
-    } catch (InterruptedException ex) {
-      // we don't care about this
-    }
-    if(ourCheckBox.isSelected()) {
+    Runnable task = new Runnable() {
+      @Override
+      public void run() {
+        // Simulate hanging Thread
+        String currentThread = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+        System.out.println("I'm going to sleep on the: " + currentThread);
+        for (int i = 10; i >= 0; i--) {
+          try {
+            int j = i;
+            Platform.runLater(() -> ourLabel.setText( ". ".repeat(j)));
+            Thread.sleep(1_000);
+
+            if(i == 0) {
+              Platform.runLater(() -> {
+                String currentThread2 = Platform.isFxApplicationThread()
+                  ? "UI Thread" : "Background Thread";
+                System.out.println("I'm updating the label on the: " + currentThread2);
+                ourLabel.setText("Background Thread done!\nBack to UI Thread");
+              });
+            }
+          } catch(InterruptedException ex){
+            // we don't care about this
+          }
+        }
+      }
+    };
+    Thread.ofVirtual().start(task);
+
+    if (ourCheckBox.isSelected()) {
       nameField.clear();
       helloButton.setDisable(true);
       byeButton.setDisable(true);
