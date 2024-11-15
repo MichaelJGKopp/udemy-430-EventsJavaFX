b4c1e6f33f141b61cfbbdf05521b1d1a87ad689b
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-15 17:39:42 +0100

    disable buttons when textField empty

--- src/main/java/dev/lpa/udemy430events/HelloController.java
+++ src/main/java/dev/lpa/udemy430events/HelloController.java
@@ -1,24 +1,39 @@
 package dev.lpa.udemy430events;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 
 public class HelloController {
 
   @FXML
   private TextField nameField;
+  @FXML
+  private Button helloButton;
+  @FXML
+  private Button byeButton;
 
   @FXML
-  protected void onHelloButtonClick() {
-    System.out.println("Hello, " + nameField.getText());
-//    nameField.setText("Welcome to JavaFX Application!");
+  public void initialize() {
+    helloButton.setDisable(true);
+    byeButton.setDisable(true);
   }
 
-  public void onEnterEmptyTextField() {
-    if (nameField.getText().equalsIgnoreCase("Enter name")) {
-      nameField.setText("");
+  @FXML
+  protected void onButtonClicked(ActionEvent e) {
+    if (e.getSource().equals(helloButton)) {
+      System.out.println("Hello, " + nameField.getText());
+    } else if (e.getSource().equals(byeButton)) {
+      System.out.println("Bye, " + nameField.getText());
     }
+  }
+
+  @FXML
+  public void handleKeyReleased() {
+    String text = nameField.getText();
+    boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+    helloButton.setDisable(disableButtons);
+    byeButton.setDisable(disableButtons);
   }
 }
